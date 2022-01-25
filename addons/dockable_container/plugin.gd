e369de80b145456489e7e3c02bc14ef67b73ed4e
Olivier <info@gaw-r.company>
2022-01-25 16:03:31 +0100

    added dockable extension

---
+++ addons/dockable_container/plugin.gd
@@ -0,0 +1,16 @@
+tool
+extends EditorPlugin
+
+const DockableContainer = preload("dockable_container.gd")
+const LayoutInspectorPlugin = preload("inspector_plugin/editor_inspector_plugin.gd")
+
+var _layout_inspector_plugin = LayoutInspectorPlugin.new()
+
+func _enter_tree() -> void:
+	add_custom_type("DockableContainer", "Container", DockableContainer, null)
+	add_inspector_plugin(_layout_inspector_plugin)
+
+
+func _exit_tree() -> void:
+	remove_inspector_plugin(_layout_inspector_plugin)
+	remove_custom_type("DockableContainer")
